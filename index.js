93480a5f553ad5834c088d049d2ada979a850ac5
javierovico <javierovico@gmail.com>
2021-04-29 19:45:43 -0400

    s

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const handler = {
     }
 };
 
-const ModeloBase = function({atributos = {}, appends = [], getters = {}}){
+const ModeloBase = function({atributos = {}, appends = [], getset = {}}){
     Object.defineProperty(this,'_attributes',{value:atributos, writable:false, enumerable:false})
     Object.defineProperty(this,'_updates',{value: {}, writable:true, enumerable:false})
-    Object.defineProperties(this,Object.entries(atributos).filter(([key])=>!(key in getters)).reduce((acum,[key,value])=> Object.assign(acum,{[key]:{
+    Object.defineProperties(this,Object.entries(atributos).filter(([key])=>!(key in getset)).reduce((acum,[key,value])=> Object.assign(acum,{[key]:{
         enumerable: true,
         // writable:true,
         // value,
@@ -26,7 +26,7 @@ const ModeloBase = function({atributos = {}, appends = [], getters = {}}){
             this._updates[key] = nuevo
         }
     }}),{}))
-    Object.defineProperties(this,Object.entries(getters).reduce((acum,[key,value])=> Object.assign(acum,{[key]:{
+    Object.defineProperties(this,Object.entries(getset).reduce((acum,[key,value])=> Object.assign(acum,{[key]:{
             enumerable: (0<=appends.indexOf(key)) || (key in atributos),
             // writable:false,
             get: function(){
@@ -35,18 +35,22 @@ const ModeloBase = function({atributos = {}, appends = [], getters = {}}){
         }}),{}))
 }
 
-const ModeloModificador = function(atributos,getters = {},appends = []){
-    return new ModeloBase({atributos,getters, appends})
-}
+// const ModeloModificador = function(atributos,getters = {},appends = []){
+//     return new ModeloBase({atributos,getters, appends})
+// }
 
 const Punto = function(atributos){
-    return new ModeloModificador(atributos,{
-        id: (target) => target._attributes.id * 2,
-        nombrePurete: (target) => target.nombre + '_ PURETE' + target.id,
-    },['nombrePurete'])
+    return new ModeloBase({
+        atributos,
+        getset:{
+            id: (target) => target._attributes.id * 2,
+            nombrePurete: (target) => target.nombre + '_ PURETE' + target.id,
+        },
+        appends:['nombrePurete']
+    })
 }
 
-const punto =Punto({id:1,nombre:'caberna'});
+const punto = new Punto({id:1,nombre:'caberna'});
 console.log(JSON.stringify(punto,null,2))
 const punto2 = Punto({id:2,nombre:'caberna2'});
 console.log(JSON.stringify(punto2,null,2))
