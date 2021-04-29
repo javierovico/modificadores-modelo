a84b1df146de12b47a10a98103380ca7c0e61ce6
javierovico <javierovico@gmail.com>
2021-04-29 00:19:11 -0400

    s

---
+++ index.js
@@ -0,0 +1,67 @@
+const handler = {
+    get: function (target, name) {
+        return name in target ?
+            target[name] :
+            37;
+    }
+};
+
+const ModeloBase = function({atributos = {}, appends = [], getters = {}}){
+    Object.defineProperty(this,'_attributes',{value:atributos, writable:false, enumerable:false})
+    Object.defineProperty(this,'_updates',{value: {}, writable:true, enumerable:false})
+    Object.defineProperties(this,Object.entries(atributos).filter(([key])=>!(key in getters)).reduce((acum,[key,value])=> Object.assign(acum,{[key]:{
+        enumerable: true,
+        // writable:true,
+        // value,
+        get: function(){
+            if(key in this._updates){
+                return this._updates[key]
+            }else if(key in this._attributes){
+                return this._attributes[key]
+            }else{
+                return undefined
+            }
+        },
+        set: function(nuevo){
+            this._updates[key] = nuevo
+        }
+    }}),{}))
+    Object.defineProperties(this,Object.entries(getters).reduce((acum,[key,value])=> Object.assign(acum,{[key]:{
+            enumerable: (0<=appends.indexOf(key)) || (key in atributos),
+            // writable:false,
+            get: function(){
+                return value(this)
+            },
+        }}),{}))
+}
+
+const ModeloModificador = function(atributos,getters = {},appends = []){
+    return new Proxy(new ModeloBase({atributos,getters, appends}),{
+
+    });
+    // return new Proxy(new ModeloBase(atributos,appends), {
+    //     get: function(target,name){
+    //         if(name in getters){
+    //             return getters[name](target)
+    //         }else if(name in target._updates){
+    //             return target._updates[name]
+    //         }else if(name in target._attributes){
+    //             return target._attributes[name]
+    //         }else if(name in target){
+    //             return target[name]
+    //         }else{
+    //             return 'nodef'
+    //         }
+    //     }
+    // })
+}
+
+const Punto = function(atributos){
+    return new ModeloModificador(atributos,{
+        id: (target) => target._attributes.id * 2,
+        nombrePurete: (target) => target.nombre + '_ PURETE' + target.id,
+    },['nombrePurete'])
+}
+
+const punto = new Punto({id:1,nombre:'caberna'});
+console.log(JSON.stringify(punto))
